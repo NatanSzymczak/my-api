98531761bf07827d4352102457c09489d915a421
Natan Szymczak <natan.szymczak@gmail.com>
2020-10-02 16:25:07 +0200

    Add uuid package from npm to create a random UUID for new user

--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,9 @@
 import express from 'express';
+import { v4 as uuidv4 } from 'uuid';
 
 const router = express.Router();
 
-const users = [
-  {
-    firstName: "John",
-    lastName: "Doe",
-    age: "25"
-  },
-  {
-    firstName: "Jane",
-    lastName: "Doe",
-    age: "24"
-  }
-]
+const users = [];
 
 // all routes in here are starting with /users
 
@@ -22,13 +12,13 @@ router.get('/', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-  console.log('POST ROUTE REACHED');
-  console.log(req.body);
-
   const user = req.body;
-  users.push(user);
-
-  res.send('POST ROUTE REACHED');
+  users.push({ ...user, id: uuidv4()});
+  res.send(`User with the name ${user.firstName} added to the database!`);
 });
+
+router.get('/:id', (req, res) => {
+  res.send('THE GET ID ROUTE');
+})
 
 export default router;
