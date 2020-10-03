de3a92e4316c831fdba8b6a22c779d7a97f60f37
Natan Szymczak <natan.szymczak@gmail.com>
2020-10-04 00:18:25 +0200

    Add the option to change user properties from the API with the 'path' method

--- routes/users.js
+++ routes/users.js
@@ -27,6 +27,18 @@ router.delete('/:id', (req, res) => {
   const {id} = req.params;
   users = users.filter( user => user.id !== id );
   res.send(`User with the id ${id} deleted from the database.`);
+})
+
+router.patch('/:id', (req,res) => {
+  const { id } = req.params;
+  const { firstName, lastName, age } = req.body;
+  const user = users.find( user => user.id === id );
+
+  if(firstName) user.firstName = firstName;
+  if(lastName) user.lastName = lastName;
+  if(age) user.age = age;
+
+  res.send(`User with the id ${id} has been updated.`);
 
 })
 
